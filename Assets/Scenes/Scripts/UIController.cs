6997a6c31f9366d376739de8d8379b86c5f153b0
wook2 <cresend01@naver.com>
2025-01-23 11:40:33 +0900

    base

---
+++ Assets/Scenes/Scripts/UIController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    private Button btnStart;
+    private GameObject lobby;
+
+    private bool firstStart = true;
+    
+    
+    void Start()
+    {
+        btnStart = GameObject.Find("BtnStart").GetComponent<Button>();
+        btnStart.onClick.AddListener(GameStart);
+        
+        lobby = GameObject.Find("Lobby");
+    }
+
+    void GameStart()
+    {
+        if (firstStart)
+        {
+            GameObject.Find("TxtTitle").GetComponent<TMP_Text>().text = "Game Over";
+            btnStart.transform.GetChild(0).GetComponent<TMP_Text>().text = "Restart";
+            firstStart = false;
+        }
+        
+        lobby.SetActive(false);
+        StartCoroutine(StartRacing());
+    }
+
+    IEnumerator StartRacing()
+    {
+        yield return null;
+    }
+
+}
